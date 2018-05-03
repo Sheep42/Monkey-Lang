2cb0c97645404ba00a6b6edd377f3979c2050328
Dan Shedd <dan@dshedd.com>
2018-05-03 00:02:41 -0400

    Parser updates
    Implemented ParseProgram, parseStatement, parseLetStatement, a few utility methods, and assertion methods

--- parser/parser.go
+++ parser/parser.go
@@ -18,8 +18,68 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+
+		return true
+	} else {
+		return false
+	}
+}
+
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement() //Parse the current statement
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt) //Add to program statements
+		}
+
+		p.nextToken() //Advance to next token
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+		case token.LET:
+			return p.parseLetStatement()
+		default:
+			return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMI) {
+		p.nextToken()
+	}
+
+	return stmt
 }
 
 func New(l *lexer.Lexer) *Parser {
