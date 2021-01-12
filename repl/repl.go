1d60f7460c6c7b8bb46dabb4e8cb55a52f09a77d
Dan Shedd <dan@dshedd.com>
2021-01-11 22:36:28 -0500

    Implement Eval in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -37,8 +38,13 @@ func Start(in io.Reader, out io.Writer) {
 
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+
+		}
 
 	}
 
