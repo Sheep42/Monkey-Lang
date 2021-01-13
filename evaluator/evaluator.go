405123e0a5d290e79da44a0adaf8c035ed25c733
Dan Shedd <dan@dshedd.com>
2021-01-12 20:14:52 -0500

    Evaluate bang operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -27,6 +27,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObj(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -56,5 +59,31 @@ func nativeBoolToBooleanObj(input bool) object.Object {
 	}
 
 	return False
+
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return Null
+	}
+
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+
+	switch right {
+	case True:
+		return False
+	case False:
+		return True
+	case Null:
+		return True
+	default:
+		return False
+	}
 
 }
