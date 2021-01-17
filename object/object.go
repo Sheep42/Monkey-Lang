7eb96e779343b466bcc83d8bb7bfaad560f1d4d5
Dan Shedd <dan@dshedd.com>
2021-01-16 20:35:07 -0500

    Implement Environment & evaluate let statements

--- object/object.go
+++ object/object.go
@@ -10,6 +10,31 @@ const (
 	ErrorObj       = "ERROR"
 )
 
+type Environment struct {
+	store map[string]Object
+}
+
+func NewEnvironment() *Environment {
+
+	s := make(map[string]Object)
+	return &Environment{store: s}
+
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+
+	obj, ok := e.store[name]
+	return obj, ok
+
+}
+
+func (e *Environment) Set(name string, val Object) Object {
+
+	e.store[name] = val
+	return val
+
+}
+
 type ObjectType string
 
 type Object interface {
